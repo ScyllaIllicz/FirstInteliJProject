719b0e4afb99e36871d57524be5f4d735bbc5e58
LinhBuiKlement <h.bui@gmx.de>
2018-09-07 15:24:28 +0200

    second commit

--- HelloWorld/src/com/example/helloworld/HelloWorld.java
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -1,9 +1,18 @@
 package com.example.helloworld;
+import java.io.IOException;
 
 public class HelloWorld {
     public static void main(String[] args){
-        PrintArgs("Was für eine umständliche Sprache!");
-        PrintArgs("");
+        byte buffer[] = new byte[80];
+        String input = "";
+        int read;
+        try {
+            read = System.in.read(buffer, 0, 80);
+            input = new String(buffer, 0, read);
+            PrintArgs(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     private static void PrintArgs(String str)
     {
